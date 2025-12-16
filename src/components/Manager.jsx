94b23bc64238afd8f32ad8d564ea8d241c90a13f
Sachin Jha <sjha04180@gmail.com>
2025-12-16 17:33:33 +0530

    Fixed saving issue

--- src/components/Manager.jsx
+++ src/components/Manager.jsx
@@ -12,7 +12,7 @@ const Manager = () => {
     const [passwordArray, setPasswordArray] = useState([])
 
     const getPasswords = async () => {
-        let req = await fetch("http://localhost:3000/")
+        let req = await fetch("https://pass-guard-mongo-db-backend.vercel.app/")
         let passwords = await req.json()
         if (passwords) {
             setPasswordArray(passwords)
@@ -43,14 +43,20 @@ const Manager = () => {
         if (form.site.length > 3 && form.username.length > 3 && form.password.length > 3) {
 
             //If any such id exists in the db, delete item
-            await fetch("http://localhost:3000/", {
-                method: "DELETE", headers: { "Content-Type": "application/json" },
-                body: JSON.stringify({ id: form.id })
-            })
+            // ✅ If editing existing password
+            if (form.id) {
+                await fetch("https://pass-guard-mongo-db-backend.vercel.app/", {
+                    method: "DELETE",
+                    headers: { "Content-Type": "application/json" },
+                    body: JSON.stringify({ id: form.id })
+                });
+            }
 
-
-            setPasswordArray([...passwordArray, { ...form, id: uuidv4() }])
-            await fetch("http://localhost:3000/", {
+            const newPassword = { ...form, id: uuidv4() };
+            
+            // ✅ Save new / updated password
+            setPasswordArray([...passwordArray, newPassword]);
+            await fetch("https://pass-guard-mongo-db-backend.vercel.app/", {
                 method: "POST", headers: { "Content-Type": "application/json" },
                 body: JSON.stringify({ ...form, id: uuidv4() })
             })
@@ -104,7 +110,7 @@ const Manager = () => {
 
             setPasswordArray(passwordArray.filter(item => item.id !== id))
             // localStorage.setItem("passwords", JSON.stringify(passwordArray.filter(item => item.id !== id)))
-            let res = await fetch("http://localhost:3000/", {
+            let res = await fetch("https://pass-guard-mongo-db-backend.vercel.app/", {
                 method: "DELETE", headers: { "Content-Type": "application/json" },
                 body: JSON.stringify({ id })
             })
